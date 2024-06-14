1785f5f580717cf8963117acfa659dfdd9f4fb58
Nari-Lee <serika9009@gmail.com>
2024-06-14 16:57:40 +0900

    15일차 수업

--- myapp/app/src/main/java/bitcamp/myapp/App1.java
+++ myapp/app/src/main/java/bitcamp/myapp/App1.java
@@ -2,112 +2,116 @@ package bitcamp.myapp;
 
 import java.util.Scanner;
 
-public class App1 {
+public class App {
 
-    static Scanner keyboardScanner = new Scanner(System.in);
+  static Scanner keyboardScanner = new Scanner(System.in);
 
-    static String[] mainMenus = new String[]{"회원", "팀", "프로젝트", "게시판", "도움말", "종료"};
-    static String[][] subMenus = {
-            {"등록", "목록", "조회", "변경", "삭제"},
-            {"등록", "목록", "조회", "변경", "삭제"},
-            {"등록", "목록", "조회", "변경", "삭제"},
-            {"등록", "목록", "조회", "변경", "삭제"}
-    };
+  static String[] mainMenus = new String[] {"회원", "팀", "프로젝트", "게시판", "도움말", "종료"};
+  static String[][] subMenus = {{"등록", "목록", "조회", "변경", "삭제"}, {"등록", "목록", "조회", "변경", "삭제"},
+      {"등록", "목록", "조회", "변경", "삭제"}, {"등록", "목록", "조회", "변경", "삭제"}};
 
-    public static void main(String[] args) {
-        printMainMenu(); // 메서드에 묶인 코드를 실행하는 것을 "메서드를 호출(call)한다"라고 부른다.
+  public static void main(String[] args) {
 
-        while (true) {
-            try {
-                String command = prompt("메인");
+    printMenu(); // 메서드에 묶인 코드를 실행하는 것을 "메서드를 호출(call)한다"라고 부른다.
 
-                if (command.equals("menu")) {
-                    printMainMenu();
-                } else {
-                    int menuNo = Integer.parseInt(command);
-                    String menuTitle = getMenuTitle(menuNo, mainMenus); // 설명하는 변수
-                    if (menuTitle == null) {
-                        System.out.println("유효한 메뉴 번호가 아닙니다.");
-                    } else if (menuTitle.equals("종료")) {
-                        break;
-                    } else {
-                        processMenu(menuTitle, mainMenus);
-                    }
-                }
-            } catch (NumberFormatException ex) {
-                System.out.println("숫자로 메뉴 번호를 입력하세요.");
-            }
-        }
+    String command;
+    while (true) {
+      try {
+        command = prompt("메인");
 
-        System.out.println("종료합니다.");
-        keyboardScanner.close();
-    }
+        if (command.equals("menu")) {
+          printMenu();
 
-    static void printMainMenu() {
-        String boldAnsi = "\033[1m";
-        String redAnsi = "\033[31m";
-        String resetAnsi = "\033[0m";
-
-        String appTitle = "[팀 프로젝트 관리 시스템]";
-        String line = "----------------------------------";
-
-        System.out.println(boldAnsi + line + resetAnsi);
-        System.out.println(boldAnsi + appTitle + resetAnsi);
-
-        for (int i = 0; i < mainMenus.length; i++) {
-            if (mainMenus[i].equals("종료")) {
-                System.out.printf("%s%d. %s%s\n", (boldAnsi + redAnsi), (i + 1), mainMenus[i], resetAnsi);
+        } else {
+          int menuNo = Integer.parseInt(command);
+          String menuTitle = getMenuTitle(menuNo, mainMenus); // 설명하는 변수
+          if (menuTitle == null) {
+            System.out.println("유효한 메뉴 번호가 아닙니다.");
+          } else if (menuTitle.equals("종료")) {
+            break;
+          } else {
+            if (menuNo >= 1 && menuNo <= 4) {
+              processMenu(menuTitle, subMenus[menuNo - 1]);
             } else {
-                System.out.printf("%d. %s\n", (i + 1), mainMenus[i]);
+              System.out.println(menuTitle);
             }
+          }
         }
-
-        System.out.println(boldAnsi + line + resetAnsi);
+      } catch (NumberFormatException ex) {
+        System.out.println("숫자로 메뉴 번호를 입력하세요.");
+      }
     }
 
-    static void printSubMenu(String menuTitle, String[] subMenus) {
-        System.out.println(menuTitle);
-        for (int i = 0; i < subMenus.length; i++) {
-            System.out.printf("%d. %s\n", (i + 1), subMenus[i]);
-        }
-        System.out.println("9, 이전");
+    System.out.println("종료합니다.");
+
+    keyboardScanner.close();
+  }
+
+  static void printMenu() {
+    String boldAnsi = "\033[1m";
+    String redAnsi = "\033[31m";
+    String resetAnsi = "\033[0m";
+
+    String appTitle = "[팀 프로젝트 관리 시스템]";
+    String line = "----------------------------------";
+
+    System.out.println(boldAnsi + line + resetAnsi);
+    System.out.println(boldAnsi + appTitle + resetAnsi);
+
+    for (int i = 0; i < mainMenus.length; i++) {
+      if (mainMenus[i].equals("종료")) {
+        System.out.printf("%s%d. %s%s\n", (boldAnsi + redAnsi), (i + 1), mainMenus[i], resetAnsi);
+      } else {
+        System.out.printf("%d. %s\n", (i + 1), mainMenus[i]);
+      }
     }
 
-    static String prompt(String title) {
-        System.out.printf("%s> ", title);
-        return keyboardScanner.nextLine();
-    }
+    System.out.println(boldAnsi + line + resetAnsi);
+  }
 
-    static boolean isValidateMenu(int menuNo, String[] menus) {
-        return menuNo >= 1 && menuNo <= menus.length;
+  static void printSubMenu(String menuTitle, String[] menus) {
+    System.out.printf("[%s]\n", menuTitle);
+    for (int i = 0; i < menus.length; i++) {
+      System.out.printf("%d. %s\n", (i + 1), menus[i]);
     }
+    System.out.println("9. 이전");
+  }
 
-    static String getMenuTitle(int menuNo, String[] menus) {
-        return isValidateMenu(menuNo, menus) ? menus[menuNo - 1] : null;
-    }
+  static String prompt(String title) {
+    System.out.printf("%s> ", title);
+    return keyboardScanner.nextLine();
+  }
 
-    static void processMenu(String menuTitle, String[] menus) {
+  static boolean isValidateMenu(int menuNo, String[] menus) {
+    return menuNo >= 1 && menuNo <= menus.length;
+  }
+
+  static String getMenuTitle(int menuNo, String[] menus) {
+    return isValidateMenu(menuNo, menus) ? menus[menuNo - 1] : null;
+  }
+
+  static void processMenu(String menuTitle, String[] menus) {
+    printSubMenu(menuTitle, menus);
+    while (true) {
+      String command = prompt("메인/" + menuTitle);
+      if (command.equals("menu")) {
         printSubMenu(menuTitle, menus);
+        continue;
+      } else if (command.equals("9")) { // 이전 메뉴 선택
+        break;
+      }
 
-        while (true) {
-            String command = prompt("메인/" + menuTitle);
-            if (command.equals("menu")) {
-                printSubMenu(menuTitle, menus);
-                continue;
-            } else if (command.equals("9")) {
-                break;
-            }
-            try {
-                int menuNo = Integer.parseInt(command);
-                String subMenuTitle = getMenuTitle(menuNo, menus);
-                if (subMenuTitle == null) {
-                    System.out.println("유효한 메뉴 번호가 아닙니다.");
-                } else {
-                    System.out.println(subMenuTitle);
-                }
-            } catch (NumberFormatException ex) {
-                System.out.println("숫자로 메뉴 번호를 입력하세요.");
-            }
+      try {
+        int menuNo = Integer.parseInt(command);
+        String subMenuTitle = getMenuTitle(menuNo, menus);
+        if (subMenuTitle == null) {
+          System.out.println("유효한 메뉴 번호가 아닙니다.");
+        } else {
+          System.out.println(subMenuTitle);
         }
+      } catch (NumberFormatException ex) {
+        System.out.println("숫자로 메뉴 번호를 입력하세요.");
+      }
     }
+  }
 }
