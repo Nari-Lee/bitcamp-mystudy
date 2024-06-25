a53d0b70b41243a9c7df3afc26bec7c3242f5199
Nari-Lee <serika9009@gmail.com>
2024-06-25 19:42:41 +0900

    22일차 수업

--- myapp/app/src/main/java/bitcamp/myapp/command/ArrayList.java
+++ myapp/app/src/main/java/bitcamp/myapp/command/ArrayList.java
@@ -1,13 +1,36 @@
 package bitcamp.myapp.command;
 
+import java.util.Arrays;
+
 public class ArrayList {
-  private static final int MAX_SIZE = 100;
+
+  private static final int MAX_SIZE = 3;
+
   protected int size = 0;
   private Object[] list = new Object[MAX_SIZE];
 
   public void add(Object obj) {
+    if (size == list.length) {
+      // 1) 우리가 만든 메서드를 사용하여 배열 크기 증가
+      //grow();
+
+      // 2) 자바에서 제공하는 클래스를 사용하여 배열 크기 증가
+      int oldSize = list.length;
+      int newSize = oldSize + (oldSize >> 1);
+      list = Arrays.copyOf(list, newSize);
+    }
     list[size++] = obj;
   }
+
+  //  private void grow() {
+  //    int oldSize = list.length;
+  //    int newSize = oldSize + (oldSize >> 1);
+  //    Object[] arr = new Object[newSize];
+  //    for (int i = 0; i < list.length; i++) {
+  //      arr[i] = list[i];
+  //    }
+  //    list = arr;
+  //  }
 
   public Object remove(int index) {
     if (index < 0 || index >= size) {
