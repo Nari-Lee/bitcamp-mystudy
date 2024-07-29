0d90403b6d7f85d013ab1eafe6edc22283164977
Nari-Lee <serika9009@gmail.com>
2024-07-29 17:18:32 +0900

    45일차 수업

---
+++ myapp/app-server/src/main/java/bitcamp/myapp/listener/InitApplicationListener.java
@@ -0,0 +1,51 @@
+package bitcamp.myapp.listener;
+
+import bitcamp.context.ApplicationContext;
+import bitcamp.listener.ApplicationListener;
+import bitcamp.myapp.dao.*;
+import bitcamp.myapp.dao.skel.UserDaoSkel;
+
+public class InitApplicationListener implements ApplicationListener {
+
+  UserDao userDao;
+  BoardDao boardDao;
+  ProjectDao projectDao;
+
+  @Override
+  public void onStart(ApplicationContext ctx) {
+    userDao = new ListUserDao("data.xlsx");
+    boardDao = new ListBoardDao("data.xlsx");
+    projectDao = new ListProjectDao("data.xlsx", userDao);
+
+    UserDaoSkel userDaoSkel = new UserDaoSkel(userDao);
+
+    ctx.setAttribute("userDaoSkel", userDaoSkel);
+  }
+
+  @Override
+  public void onShutdown(ApplicationContext ctx) throws Exception {
+    try {
+      ((ListUserDao) userDao).save();
+    } catch (Exception e) {
+      System.out.println("회원 데이터 저장 중 오류 발생!");
+      e.printStackTrace();
+      System.out.println();
+    }
+
+    try {
+      ((ListBoardDao) boardDao).save();
+    } catch (Exception e) {
+      System.out.println("게시글 데이터 저장 중 오류 발생!");
+      e.printStackTrace();
+      System.out.println();
+    }
+
+    try {
+      ((ListProjectDao) projectDao).save();
+    } catch (Exception e) {
+      System.out.println("프로젝트 데이터 저장 중 오류 발생!");
+      e.printStackTrace();
+      System.out.println();
+    }
+  }
+}
